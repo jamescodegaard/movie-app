f95b8383fe6328f0bf00996557632c76341b6bcd
jamescodegaard <jamescodegaard@gmail.com>
2020-05-29 15:20:26 -0500

    adds Movie model, seeded movies, and movies controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@
 
 # actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 # actor.save
+
+# new_movie = Movie.new(title: "The Shining", year: 1980, plot: "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future. -IMDB Description")
+# new_movie.save
+
+# new_movie = Movie.new(title: "Bring It On", year: 2000, plot: "A champion high school cheerleading squad discovers its previous captain stole all their best routines from an inner-city school and must scramble to compete at this year's championships. -IMDB Description")
+# new_movie.save
+
+# new_movie = Movie.new(title: "Doctor Sleep", year: 2019, plot: "Years following the events of The Shining (1980), a now-adult Dan Torrance must protect a young girl with similar powers from a cult known as The True Knot, who prey on children with powers to remain immortal. -IMDB Description")
+# new_movie.save
+
+# new_movie = Movie.new(title: "Gladiator", year: 2000, plot: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery. -IMDB Description")
+# new_movie.save
