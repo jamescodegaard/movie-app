f3feebde42c0dc48371183734392682e57da75fe
jamescodegaard <jamescodegaard@gmail.com>
2020-06-08 10:31:45 -0500

    adds validations and happy/sad path

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -13,8 +13,11 @@ class Api::MoviesController < ApplicationController
       director: params[:director],
       english: params[:english]
     )
-    @movie.save
-    render "show.json.jb"
+    if @movie.save #happy
+      render 'show.json.jb'
+    else #sad
+      render json: {error: @movie.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -29,8 +32,11 @@ class Api::MoviesController < ApplicationController
     @movie.plot = params[:plot] || @movie.plot
     @movie.director = params[:director] || @movie.director
     @movie.english = params[:english] || @movie.english
-    @movie.save
-    render "show.json.jb"
+    if @movie.save #happy
+      render 'show.json.jb'
+    else #sad
+      render json: {error: @movie.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :plot, length: { in: 10...500 }
 end
