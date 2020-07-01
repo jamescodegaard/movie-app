7264a632bd8a0b39cdb6cdaf6c8b5db5228373b7
jamescodegaard <jamescodegaard@gmail.com>
2020-07-01 17:46:29 -0500

    updates seed file before heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-# actor.save
-
-# new_movie = Movie.new(title: "The Shining", year: 1980, plot: "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future. -IMDB Description")
-# new_movie.save
-
-# new_movie = Movie.new(title: "Bring It On", year: 2000, plot: "A champion high school cheerleading squad discovers its previous captain stole all their best routines from an inner-city school and must scramble to compete at this year's championships. -IMDB Description")
-# new_movie.save
-
-# new_movie = Movie.new(title: "Doctor Sleep", year: 2019, plot: "Years following the events of The Shining (1980), a now-adult Dan Torrance must protect a young girl with similar powers from a cult known as The True Knot, who prey on children with powers to remain immortal. -IMDB Description")
-# new_movie.save
-
-# new_movie = Movie.new(title: "Gladiator", year: 2000, plot: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery. -IMDB Description")
-# new_movie.save
-
-genres = Genre.create([
-  { name: 'Comedy' }, 
-  { name: 'Horror' }, 
-  { name: 'Thriller' }, 
-  { name: 'Action' }, 
-  { name: 'Romantic Comedy'},
-  { name: 'SciFi'},
-  { name: 'Drama'}
-  ])
-
-movie_genres = MovieGenre.create([
-  { movie_id: 1, genre_id: 1}, 
-  { movie_id: 2, genre_id: 6}, 
-  { movie_id: 3, genre_id: 3}, 
-  { movie_id: 4, genre_id: 2}, 
-  { movie_id: 4, genre_id: 3},
-  { movie_id: 6, genre_id: 3},
-  { movie_id: 8, genre_id: 1},
-  { movie_id: 9, genre_id: 5},
-  { movie_id: 11, genre_id: 2},
-  { movie_id: 10, genre_id: 7}
-  ])
+Movie.create!([
+  {title: "Romy & Michele's High School Reunion", year: 1997, plot: "Two dim-witted, inseparable friends hit the road for their ten-year high school reunion and concoct an elaborate lie about their lives in order to impress their classmates. -IMDB Description", director: nil, english: true},
+  {title: "Contact", year: 1997, plot: "Dr. Ellie Arroway, after years of searching, finds conclusive radio proof of extraterrestrial intelligence, sending plans for a mysterious machine. -IMDB Description", director: nil, english: true},
+  {title: "Bring It On", year: 2000, plot: "A champion high school cheerleading squad discovers its previous captain stole all their best routines from an inner-city school and must scramble to compete at this year's championships. -IMDB Description", director: nil, english: true},
+  {title: "Doctor Sleep", year: 2019, plot: "Years following the events of The Shining (1980), a now-adult Dan Torrance must protect a young girl with similar powers from a cult known as The True Knot, who prey on children with powers to remain immortal. -IMDB Description", director: nil, english: true},
+  {title: "To Wong Foo", year: 1995, plot: "Three drag queens travel cross-country until their car breaks down, leaving them stranded in a small town. -IMDB Description", director: nil, english: true},
+  {title: "Knives Out", year: 2019, plot: "A detective investigates the death of a patriarch of an eccentric, combative family. - IMDB Description", director: "Rian Johnson", english: true},
+  {title: "The Shining", year: 1980, plot: "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future. -IMDB Description", director: nil, english: false},
+  {title: "The Devil Wears Prada", year: 2006, plot: "A smart but sensible new graduate lands a job as an assistant to Miranda Priestly, the demanding editor-in-chief of a high fashion magazine. -IMDB Description", director: "David Frankel", english: true},
+  {title: "The Imitation Game", year: 2014, plot: "During World War II, the English mathematical genius Alan Turing tries to crack the German Enigma code with help from fellow mathematicians. -IMDB Description", director: "Morten Tyldum", english: true},
+  {title: "A Quiet Place", year: 2018, plot: "In a post-apocalyptic world, a family is forced to live in silence while hiding from monsters with ultra-sensitive hearing. -IMDB Description", director: "John Krasinski", english: true}
+])
+Actor.create!([
+  {first_name: "Emily", last_name: "Blunt", known_for: "The Devil Wear's Prada", gender: "F", age: 37, movie_id: 8},
+  {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock", gender: "M", age: 43, movie_id: 9},
+  {first_name: "John", last_name: "Krasinski", known_for: "A Quiet Place", gender: "M", age: 40, movie_id: 10},
+  {first_name: "RuPaul", last_name: "Charles", known_for: "To Wong Foo", gender: "M", age: 59, movie_id: 5}
+])
+Genre.create!([
+  {name: "Comedy"},
+  {name: "Horror"},
+  {name: "Thriller"},
+  {name: "Action"},
+  {name: "Romantic Comedy"},
+  {name: "SciFi"},
+  {name: "Drama"}
+])
+MovieGenre.create!([
+  {genre_id: 1, movie_id: 1},
+  {genre_id: 6, movie_id: 2},
+  {genre_id: 1, movie_id: 3},
+  {genre_id: 3, movie_id: 4},
+  {genre_id: 2, movie_id: 4},
+  {genre_id: 3, movie_id: 5},
+  {genre_id: 1, movie_id: 6},
+  {genre_id: 5, movie_id: 7},
+  {genre_id: 2, movie_id: 8},
+  {genre_id: 4, movie_id: 10},
+  {genre_id: 2, movie_id: 10}
+])
+User.create!([
+  {name: "kameron", email: "k@gmail.com", password_digest: "$2a$12$Mlwm3i01adhquY0WR9MG7OQIvrGxYwqaHG0Ahh1oTGZ7kdZ/eaDwK", admin: false},
+  {name: "james", email: "james@gmail.com", password_digest: "$2a$12$fQNJTLk8VJWZBCCxJK/ktusl742z200u5hfEMHeUiW8GGO.fSyMIu", admin: true}
+])
